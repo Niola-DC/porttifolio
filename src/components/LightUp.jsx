1ef44d1b0a729a380c553dfd8aa6250d089eac19
GITHUB-USERNAME <itzdonnacruz00@gmail.com>
2025-04-12 09:07:49 -0500

    text adjustment

--- src/components/LightUp.jsx
+++ src/components/LightUp.jsx
@@ -6,7 +6,7 @@ const LightUp = () => {
                 <main>
                     <div className="flex-grow flex items-center justify-center mt-12 md:mt-16 lg:mt-20 mb-10">
                         <div className="text-center max-w-4xl px-4">
-                            <h1 className=" font-mali text-4xl md:text-5xl lg:text-6xl font-normal text-gray-900 leading-tight hover:text-white transition-colors duration-300 ease-in-out">
+                            <h1 className=" font-mali text-4xl md:text-5xl lg:text-6xl font-normal text-gray-400 lg:text-gray-900 leading-tight lg:hover:text-white transition-colors duration-300 ease-in-out">
                                 Eniola Fanegan is an independent software developer, builder, and creative problem solver. I craft intuitive digital experiences and help ideas come to life through clean code, thoughtful design, and tech that works.
                             </h1>
                         </div>
